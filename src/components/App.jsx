4da45bc80a60bcdc46d71cc54b8a6aa9e3ada363
Svitlana-Hreshnova <133237028+Svitlana-Hreshnova@users.noreply.github.com>
2023-11-25 17:03:44 +0100

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,27 @@
-import Profile from './Profile/Profile'
-import user from '../user.json'
+import Profile from './Profile/Profile';
+import user from 'data/user.json';
 import Container from './Container/Container';
+import data from 'data/data.json';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import friends from 'data/friends.json';
 
 const App = () => {
   return (
     <>
       <Container>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
         />
-        </Container>
+        <Statistics title="Upload stats" stats={data} />
+        <FriendList friends={friends} />
+      </Container>
     </>
-  )
+  );
 };
 
 export default App;
-
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,21 @@
+import css from './FriendList.module.css';
+import FriendListItem from '../FriendListItem/FriendListItem';
+import PropTypes from 'prop-types';
+
+const FriendList = ({ friends }) => (
+  <ul className={css.friendList}>
+    {friends.map(({ id, ...friend }) => (
+      <FriendListItem key={id} {...friend} />
+    ))}
+  </ul>
+);
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default FriendList;
