7dc0e9efa847c6d3d64c6c49f100e3343f369bbb
Svitlana-Hreshnova <133237028+Svitlana-Hreshnova@users.noreply.github.com>
2023-11-26 19:07:38 +0100

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,8 @@ import data from 'data/data.json';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
 import friends from 'data/friends.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from 'data/transactions.json';
 
 const App = () => {
   return (
@@ -19,6 +21,7 @@ const App = () => {
         />
         <Statistics title="Upload stats" stats={data} />
         <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
       </Container>
     </>
   );
